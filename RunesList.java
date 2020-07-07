b947d703128ba356c4205578970746cd1140095a
tphsmp <40296154+tphsmp@users.noreply.github.com>
2020-07-07 12:17:43 +0600

    Update RunesList.java

--- RunesList.java
+++ RunesList.java
@@ -44,5 +44,33 @@ public class RunesList extends Runa {
             System.out.println(i + 1 + " " + str + " " + runesList.get(i));
         }
     }
+    
+    public void makeRunes(RunesList runesList) {
+        runesList.addRunes(new Runa("FEHU", "", 1, 2));
+        runesList.addRunes(new Runa("URUZ", "", 2, 2));
+        runesList.addRunes(new Runa("THURISAZ", "", 3, 2));
+        runesList.addRunes(new Runa("ANSUZ", "", 4, 2));
+        runesList.addRunes(new Runa("RAIDO", "", 5, 2));
+        runesList.addRunes(new Runa("KENAZ", "", 6, 2));
+        runesList.addRunes(new Runa("GIFU", "", 7, 1));
+        runesList.addRunes(new Runa("WUNJO", "", 8, 2));
+        runesList.addRunes(new Runa("HAGALAZ", "", 9, 1));
+        runesList.addRunes(new Runa("NAUTIZ", "", 10, 1));
+        runesList.addRunes(new Runa("ISA", "", 11, 1));
+        runesList.addRunes(new Runa("JERA", "", 12, 1));
+        runesList.addRunes(new Runa("EIHWAZ", "", 13, 1));
+        runesList.addRunes(new Runa("PERTHRO", "", 14, 2));
+        runesList.addRunes(new Runa("ALGIZ", "", 15, 2));
+        runesList.addRunes(new Runa("SIEGEL", "", 16, 1));
+        runesList.addRunes(new Runa("TYR", "", 17, 2));
+        runesList.addRunes(new Runa("BERKANA", "", 18, 2));
+        runesList.addRunes(new Runa("EHWAZ", "", 19, 2));
+        runesList.addRunes(new Runa("MANNAZ", "", 20, 2));
+        runesList.addRunes(new Runa("LAGUZ", "", 21, 2));
+        runesList.addRunes(new Runa("INGWAZ", "", 22, 1));
+        runesList.addRunes(new Runa("DAGAZ", "", 23, 1));
+        runesList.addRunes(new Runa("OTHAL", "", 24, 2));
+        runesList.addRunes(new Runa("WYRD", "", 25, 1));
+    }
 
 }
